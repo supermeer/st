22a0cfce226793f1ad464ff930ec2b302ebd2883
caoyc <supermeer@163.com>
2025-12-19 15:08:43 +0800

    feat:提示语相关修改

--- components/chat/role-msg/btns/index.js
+++ components/chat/role-msg/btns/index.js
@@ -6,12 +6,28 @@ Component({
       value: false
     }
   },
-  data: {},
+  lifetimes: {
+    attached: function () {
+      const tipMark = wx.getStorageSync('tipMark')
+      this.setData({
+        showTip: !tipMark
+      })
+    }
+  },
+  data: {
+    showTip: false
+  },
   methods: {
     onBtnClick(e) {
       // 直接触发事件，让父组件 role-msg 统一处理 disabled 逻辑
       const action = e.currentTarget.dataset.action;
       this.triggerEvent('buttonClick', { action, current: this });
+    },
+    closeTip() {
+      this.setData({
+        showTip: false
+      })
+      wx.setStorageSync('tipMark', true)
     }
   }
 })
