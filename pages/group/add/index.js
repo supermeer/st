802f5bc7632958b52d4ed1e1adfb69ba0e85d0cc
supermeer <supermeer@163.com>
2026-06-24 21:44:16 +0800

    feat:创建群聊页面开发

--- pages/group/add/index.js
+++ pages/group/add/index.js
@@ -1,9 +1,4 @@
 import SystemInfo from '../../../utils/system'
-import { 
-  getCharacterTag,
-  getCurrentPlotByCharacterId
-} from '../../../services/role/index'
-import { verifyUrls } from '../../../services/file/index'
 
 Page({
   /**
@@ -25,24 +20,11 @@ Page({
       userAddressedAs: '',
       identity: '',
       personaGender: '',
-      backgroundImage: null,
-      tagIds: []
-    },
-    voiceForm: {
-      showMark: false,
-      voiceId: null,
-      voiceName: ''
+      backgroundImage: null
     },
     showUploader: false,
     currentBg: '',
     
-    tagOptions: [],
-    selectedTagList: [],
-    maxTagSelect: 4,
-    showTagSelector: false,
-    tempTagIds: [],
-    tagSelectorOptions: [],
-
     selectedCharacters: []
   },
 
@@ -50,23 +32,23 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    const voiceMark = wx.getStorageSync('voiceNewMark')
-    if (!voiceMark) {
-      this.setData({
-        'voiceForm.showMark': true
-      })
-    }
-
     this.setData({
       pageInfo: { ...this.data.pageInfo, ...SystemInfo.getPageInfo() }
     })
-
-    this.initMetaOptions()
 
     const nav = this.selectComponent('#groupAddNav')
     if (nav) {
       nav.setBackAction(this.backAction)
     }
+
+    // 测试数据
+    this.setData({
+      selectedCharacters: [
+        { id: 1, name: '测试角色1', avatarUrl: 'https://picsum.photos/200' },
+        { id: 2, name: '测试角色2', avatarUrl: 'https://picsum.photos/201' },
+        { id: 3, name: '测试角色3', avatarUrl: '' }
+      ]
+    })
   },
 
   backAction() {
@@ -82,143 +64,6 @@ Page({
         wx.navigateBack()
       }
     })
-  },
-
-  initMetaOptions() {
-    getCharacterTag()
-      .then((tagOptions) => {
-        this.setData(
-          {
-            tagOptions: Array.isArray(tagOptions) ? tagOptions : []
-          },
-          () => {
-            this.syncSelectedMeta()
-          }
-        )
-      })
-      .catch(() => {})
-  },
-
-  normalizeIdList(list) {
-    return (Array.isArray(list) ? list : [])
-      .map((v) => Number(v))
-      .filter((v) => !Number.isNaN(v))
-  },
-
-  buildSelectedOptions(options, selectedIds) {
-    const set = new Set(this.normalizeIdList(selectedIds))
-    return (Array.isArray(options) ? options : []).map((o) => ({
-      ...o,
-      selected: set.has(Number(o.id))
-    }))
-  },
-
-  syncSelectedMeta() {
-    const { tagOptions, formData } = this.data
-    const tagIds = this.normalizeIdList(formData.tagIds)
-
-    const selectedTagList = (tagOptions || []).filter(
-      (t) => tagIds.indexOf(Number(t.id)) !== -1
-    )
-
-    const tagSelectorOptions = this.buildSelectedOptions(
-      this.data.tagOptions,
-      this.data.tempTagIds
-    )
-
-    this.setData({
-      selectedTagList,
-      tagSelectorOptions,
-      'formData.tagIds': tagIds
-    })
-  },
-
-  onOpenTagSelector() {
-    const tagIds = this.normalizeIdList(this.data.formData.tagIds)
-    const tagSelectorOptions = this.buildSelectedOptions(
-      this.data.tagOptions,
-      tagIds
-    )
-    this.setData({
-      showTagSelector: true,
-      tempTagIds: tagIds,
-      tagSelectorOptions
-    })
-  },
-
-  onCloseTagSelector() {
-    this.setData({
-      showTagSelector: false
-    })
-  },
-
-  onToggleTagInSelector(e) {
-    const { id } = e.currentTarget.dataset
-    const normalizedId = Number(id)
-    if (Number.isNaN(normalizedId)) {
-      return
-    }
-    const tempTagIds = [...this.normalizeIdList(this.data.tempTagIds)]
-    const idx = tempTagIds.indexOf(normalizedId)
-    if (idx !== -1) {
-      tempTagIds.splice(idx, 1)
-      this.setData({
-        tempTagIds,
-        tagSelectorOptions: this.buildSelectedOptions(
-          this.data.tagOptions,
-          tempTagIds
-        )
-      })
-      return
-    }
-    if (tempTagIds.length >= this.data.maxTagSelect) {
-      wx.showToast({
-        title: `最多选择${this.data.maxTagSelect}个标签`,
-        icon: 'none'
-      })
-      return
-    }
-    tempTagIds.push(normalizedId)
-    this.setData({
-      tempTagIds,
-      tagSelectorOptions: this.buildSelectedOptions(
-        this.data.tagOptions,
-        tempTagIds
-      )
-    })
-  },
-
-  onConfirmTagSelector() {
-    this.setData(
-      {
-        'formData.tagIds': [...(this.data.tempTagIds || [])],
-        showTagSelector: false
-      },
-      () => {
-        this.syncSelectedMeta()
-      }
-    )
-  },
-
-  onRemoveSelectedTag(e) {
-    const { id } = e.currentTarget.dataset
-    const normalizedId = Number(id)
-    if (Number.isNaN(normalizedId)) {
-      return
-    }
-    const tagIds = [...(this.data.formData.tagIds || [])]
-    const idx = tagIds.indexOf(normalizedId)
-    if (idx !== -1) {
-      tagIds.splice(idx, 1)
-      this.setData(
-        {
-          'formData.tagIds': tagIds
-        },
-        () => {
-          this.syncSelectedMeta()
-        }
-      )
-    }
   },
 
   onTouchMove() {},
@@ -256,6 +101,19 @@ Page({
   },
 
   /**
+   * 移除群成员
+   */
+  onRemoveCharacter(e) {
+    const { id } = e.currentTarget.dataset
+    const selectedCharacters = [...this.data.selectedCharacters]
+    const index = selectedCharacters.findIndex((c) => c.id === id)
+    if (index !== -1) {
+      selectedCharacters.splice(index, 1)
+      this.setData({ selectedCharacters })
+    }
+  },
+
+  /**
    * 添加角色
    */
   onAddCharacters() {
@@ -280,28 +138,6 @@ Page({
       'formData.userAddressedAs': userAddressedAs,
       'formData.identity': identity,
       'formData.personaGender': personaGender
-    })
-  },
-
-  /**
-   * 角色声音
-   */
-  onRoleVoice() {
-    wx.setStorageSync('voiceNewMark', true)
-    this.setData({
-      'voiceForm.showMark': false
-    })
-    wx.navigateTo({
-      url: `/pages/role/voice-list/index?characterId=${this.data.formData.id || ''}&voiceId=${this.data.voiceForm.voiceId || ''}&from=group`
-    })
-  },
-
-  confirmRoleVoice(voice) {
-    this.setData({
-      voiceForm: {
-        ...this.data.voiceForm,
-        ...voice
-      }
     })
   },
 
